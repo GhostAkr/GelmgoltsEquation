e6f5a1ea8de1752a620baff2f03ea4fd6466dd8b
Alexander <ighost99@gmail.com>
2019-10-14 18:35:40 +0300

    rightPart was renamed to f (it only returns value in (x, y) point). New rightPart was added. Now it calculates full matrix consisting of right parts. checkResult was added. First try to make Jacobi (NOT working yet!).

--- project/include/GelmgoltsEquation.h
+++ project/include/GelmgoltsEquation.h
@@ -5,17 +5,20 @@
 using namespace std;
 
 // Main
-void Zeidel(double** _mesh, int _rows, int _cols, double _k);  // TODO: make Zeidel
-void Jacobi(double** _mesh, int _rows, int _cols, double _k);  // TODO: make Jacobi
-double rightPart(double _x, double _y, double _k);
+void Zeidel(double** _mesh, int _rows, int _cols, double _k, double _step);  // TODO: make Zeidel
+void Jacobi(double** _mesh, int _rows, int _cols, double _k, double _step);  // TODO: make Jacobi
+double f(double _x, double _y, double _k);
 double exactSolution(double _x, double _y);
+double** rightPart(double _step, double _rows, double _cols, double _k);
 
 // Additional
 double** createMesh(double _xBorder, double _yBorder, double _step, double* _rows, double* _cols);  // Step must be valid
 void zeroLayer(double** _mesh, int _rows, int _cols);
+double** copyMesh(double** _mesh, int _rows, int _cols);
+bool checkResult(double** _result, int _rows, int _cols, double _step);
 
 // Boundaries
-void leftBoundary(double** _mesh, int _rows, int _cols, double _boundValue = 0.0);
-void rightBoundary(double** _mesh, int _rows, int _cols, double _boundValue = 0.0);
-void topBoundary(double** _mesh, int _rows, int _cols, double _boundValue = 0.0);
-void bottomBoundary(double** _mesh, int _rows, int _cols, double _boundValue = 0.0);
+void leftBoundary(double** _mesh, int _rows, int _cols, double _boundValue);
+void rightBoundary(double** _mesh, int _rows, int _cols, double _boundValue);
+void topBoundary(double** _mesh, int _rows, int _cols, double _boundValue);
+void bottomBoundary(double** _mesh, int _rows, int _cols, double _boundValue);
--- project/src/GelmgoltsEquation.cpp
+++ project/src/GelmgoltsEquation.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 // Main
 
-double rightPart(double _x, double _y, double _k) {
+double f(double _x, double _y, double _k) {
 	return 2.0 * sin(Pi * _y) + _k * _k * (1 - _x) * _x * sin(Pi * _y) + \
 		Pi * Pi * (1 - _x) * _x * sin(Pi * _y);
 }
@@ -17,8 +17,44 @@ double exactSolution(double _x, double _y) {
 	return (1 - _x) * _x * sin(Pi * _y);
 }
 
-void Jacobi(double** _mesh, int _rows, int _cols, double _k) {
+double** rightPart(double _step, double _rows, double _cols, double _k) {
+	double** result = createMatr(_rows, _cols);
+	for (int i = 1; i < _rows - 1; ++i) {
+		for (int j = 1; j < _cols - 1; ++j) {
+			result[i][j] = f(i * _step, j * _step, _k);
+		}
+	}
+	double boundaryValue = 0.0;
+	/*leftBoundary(result, _rows, _cols, boundaryValue);
+	rightBoundary(result, _rows, _cols, boundaryValue);
+	topBoundary(result, _rows, _cols, boundaryValue);
+	bottomBoundary(result, _rows, _cols, boundaryValue);*/
+	return result;
+}
 
+void Jacobi(double** _mesh, int _rows, int _cols, double _k, double _step) {
+	double** rPart = rightPart(_step, _rows, _cols, _k);
+	//double** previousLayer = nullptr;
+	for (int s = 0; s < 42; ++s) {
+		cout << "_mesh[1][1] = " << _mesh[1][1] << endl;
+		cout << "_mesh[2][1] = " << _mesh[2][1] << endl;
+		double** previousLayer = copyMesh(_mesh, _rows, _cols);
+		for (int i = 1; i < _rows - 1; ++i) {
+			for (int j = 1; j < _cols - 1; ++j) {
+				_mesh[i][j] = 0.25 * (previousLayer[i - 1][j] + previousLayer[i + 1][j] + previousLayer[i][j - 1] + \
+					previousLayer[i][j + 1] - _k * _k * _step * _step * previousLayer[i][j]) + rPart[i][j];
+			}
+		}
+		deleteMatr(previousLayer, _rows);
+	}
+	if (checkResult(_mesh, _rows, _cols, _step)) {
+		cout << "Answer is correct" << endl;
+	}
+	else {
+		cout << "Answer is INcorrect" << endl;
+	}
+	deleteMatr(rPart, _rows);
+	//deleteMatr(previousLayer, _rows);
 }
 
 
@@ -43,33 +79,60 @@ void zeroLayer(double** _mesh, int _rows, int _cols) {
 			_mesh[i][j] = zeroValue;
 		}
 	}
-	leftBoundary(_mesh, _rows, _cols);
-	rightBoundary(_mesh, _rows, _cols);
-	topBoundary(_mesh, _rows, _cols);
-	bottomBoundary(_mesh, _rows, _cols);
+	double boundaryValue = 0.0;
+	/*leftBoundary(_mesh, _rows, _cols, boundaryValue);
+	rightBoundary(_mesh, _rows, _cols, boundaryValue);
+	topBoundary(_mesh, _rows, _cols, boundaryValue);
+	bottomBoundary(_mesh, _rows, _cols, boundaryValue);*/
+}
+
+double** copyMesh(double** _mesh, int _rows, int _cols) {
+	double** result = createMatr(_rows, _cols);
+	for (int i = 0; i < _rows; ++i) {
+		for (int j = 0; j < _cols; ++j) {
+			_mesh[i][j] = result[i][j];
+		}
+	}
+	return result;
+}
+
+bool checkResult(double** _result, int _rows, int _cols, double _step) {
+	double epsNull = 1e-5;
+	for (int i = 0; i < _rows; ++i) {
+		for (int j = 0; j < _cols; ++j) {
+			if (fabs(_result[i][j] - exactSolution(i * _step, j * _step)) > epsNull) {
+				cout << "_result[i][j] = " << _result[i][j] << endl;
+				cout << "Exact = " << exactSolution(i * _step, j * _step) << endl;
+				cout << "i = " << i << endl;
+				cout << "j = " << j << endl;
+				return false;
+			}
+		}
+	}
+	return true;
 }
 
 // Boundaries
 
-void leftBoundary(double** _mesh, int _rows, int _cols, double _boundValue = 0.0) {
+void leftBoundary(double** _mesh, int _rows, int _cols, double _boundValue) {
 	for (int i = 0; i < _rows; ++i) {
 		_mesh[i][0] = _boundValue;
 	}
 }
 
-void rightBoundary(double** _mesh, int _rows, int _cols, double _boundValue = 0.0) {
+void rightBoundary(double** _mesh, int _rows, int _cols, double _boundValue) {
 	for (int i = 0; i < _rows; ++i) {
 		_mesh[i][_cols - 1] = _boundValue;
 	}
 }
 
-void topBoundary(double** _mesh, int _rows, int _cols, double _boundValue = 0.0) {
+void topBoundary(double** _mesh, int _rows, int _cols, double _boundValue) {
 	for (int i = 0; i < _cols; ++i) {
 		_mesh[_rows - 1][i] = _boundValue;
 	}
 }
 
-void bottomBoundary(double** _mesh, int _rows, double _cols, double _boundValue = 0.0) {
+void bottomBoundary(double** _mesh, int _rows, double _cols, double _boundValue) {
 	for (int i = 0; i < _cols; ++i) {
 		_mesh[0][i] = _boundValue;
 	}
