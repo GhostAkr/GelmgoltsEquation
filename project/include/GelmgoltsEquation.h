d72c160e48d4175aab404c85ef6a24499a7381ca
Alexander <ighost99@gmail.com>
2019-10-14 10:13:55 +0300

    zeroLayer was added. Boundaries were added.

--- project/include/GelmgoltsEquation.h
+++ project/include/GelmgoltsEquation.h
@@ -4,4 +4,16 @@
 
 using namespace std;
 
+// Main
+double** Zeidel(double** _mesh, int _rows, int _cols);  // TODO: make Zeidel
+double** Jacobi(double** _mesh, int _rows, int _cols);  // TODO: make Jacobi
+
+// Additional
 double** createMesh(double _xBorder, double _yBorder, double _step, double* _rows, double* _cols);  // Step must be valid
+void zeroLayer(double** _mesh, int _rows, int _cols);
+
+// Boundaries
+void leftBoundary(double** _mesh, int _rows, int _cols, double _boundValue = 0.0);
+void rightBoundary(double** _mesh, int _rows, int _cols, double _boundValue = 0.0);
+void topBoundary(double** _mesh, int _rows, int _cols, double _boundValue = 0.0);
+void bottomBoundary(double** _mesh, int _rows, int _cols, double _boundValue = 0.0);
--- project/src/GelmgoltsEquation.cpp
+++ project/src/GelmgoltsEquation.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+// Additional
+
 double** createMesh(double _xBorder, double _yBorder, double _step, double* _rows, double* _cols) {
 	double zeroPoint = 0.0;
 	// Step must be valid. Here is no exception for it!
@@ -12,4 +14,43 @@ double** createMesh(double _xBorder, double _yBorder, double _step, double* _row
 	*_cols = cols;
 	double** resMesh = createMatr(rows, cols);
 	return resMesh;
+}
+
+void zeroLayer(double** _mesh, int _rows, int _cols) {
+	double zeroValue = 0.0;
+	for (int i = 0; i < _rows; ++i) {
+		for (int j = 0; j < _cols; ++j) {
+			_mesh[i][j] = zeroValue;
+		}
+	}
+	leftBoundary(_mesh, _rows, _cols);
+	rightBoundary(_mesh, _rows, _cols);
+	topBoundary(_mesh, _rows, _cols);
+	bottomBoundary(_mesh, _rows, _cols);
+}
+
+// Boundaries
+
+void leftBoundary(double** _mesh, int _rows, int _cols, double _boundValue = 0.0) {
+	for (int i = 0; i < _rows; ++i) {
+		_mesh[i][0] = _boundValue;
+	}
+}
+
+void rightBoundary(double** _mesh, int _rows, int _cols, double _boundValue = 0.0) {
+	for (int i = 0; i < _rows; ++i) {
+		_mesh[i][_cols - 1] = _boundValue;
+	}
+}
+
+void topBoundary(double** _mesh, int _rows, int _cols, double _boundValue = 0.0) {
+	for (int i = 0; i < _cols; ++i) {
+		_mesh[_rows - 1][i] = _boundValue;
+	}
+}
+
+void bottomBoundary(double** _mesh, int _rows, double _cols, double _boundValue = 0.0) {
+	for (int i = 0; i < _cols; ++i) {
+		_mesh[0][i] = _boundValue;
+	}
 }
