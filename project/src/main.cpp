eec3cbb5b2e986f755a9ac58f51c0342e8c19905
Alexander Antoshkin <ighost99@gmail.com>
2019-10-15 20:34:38 +0300

    Jacobi was improved.

--- project/src/main.cpp
+++ project/src/main.cpp
@@ -13,7 +13,7 @@ int main() {
 	double t1 = omp_get_wtime();
 	Jacobi(mesh, rows, cols, 1, step);
 	double t2 = omp_get_wtime();
-	cout << "Timesa spent (Jacobi): " << t2 - t1 << endl;
+	cout << "Time spent (Jacobi): " << t2 - t1 << endl;
 	cout << "Zeidel" << endl;
 	t1 = omp_get_wtime();
 	Zeidel(mesh, rows, cols, 1, step);
