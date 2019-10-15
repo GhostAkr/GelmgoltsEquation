50a6dfef19db1d7b4d9cc30efa70c5b69beff21b
Alexander Antoshkin <ighost99@gmail.com>
2019-10-15 19:57:13 +0300

    Zeidel was added.

--- project/src/main.cpp
+++ project/src/main.cpp
@@ -1,5 +1,6 @@
 #include "../include/Matr.h"
 #include "../include/GelmgoltsEquation.h"
+#include <omp.h>
 
 using namespace std;
 
@@ -8,7 +9,16 @@ int main() {
 	double cols = 0.0;
 	double step = 0.01;
 	double** mesh = createMesh(1, 1, step, &rows, &cols);
+	cout << "Jacobi" << endl;
+	double t1 = omp_get_wtime();
 	Jacobi(mesh, rows, cols, 1, step);
+	double t2 = omp_get_wtime();
+	cout << "Timesa spent (Jacobi): " << t2 - t1 << endl;
+	cout << "Zeidel" << endl;
+	t1 = omp_get_wtime();
+	Zeidel(mesh, rows, cols, 1, step);
+	t2 = omp_get_wtime();
+	cout << "Time spent (Zeidel):" << t2 - t1 << endl;
 	//printMatr(mesh, rows, cols);
 	deleteMatr(mesh, rows);
 	system("pause");
