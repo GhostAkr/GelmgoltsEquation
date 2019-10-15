4ac8591624b203d83e8c1b2ab5033bab37a0de5f
Alexander Antoshkin <ighost99@gmail.com>
2019-10-15 18:41:33 +0300

    Counter version of Jacobi method was added (commented). Jacobi was added. copyMesh was fixed. rightPart was renamed to f (it only returns value in (x, y) point). New rightPart was added. Now it calculates full matrix consisting of right parts. checkResult was added. exactSolution was added.

--- project/src/GelmgoltsEquation.cpp
+++ project/src/GelmgoltsEquation.cpp
@@ -1,6 +1,7 @@
 #include "../include/GelmgoltsEquation.h"
 #include "../include/Matr.h"
 #include <cmath>
+#include <omp.h>
 
 #define Pi 3.14
 
@@ -8,14 +9,96 @@ using namespace std;
 
 // Main
 
-double rightPart(double _x, double _y, double _k) {
+double f(double _x, double _y, double _k) {
 	return 2.0 * sin(Pi * _y) + _k * _k * (1 - _x) * _x * sin(Pi * _y) + \
 		Pi * Pi * (1 - _x) * _x * sin(Pi * _y);
 }
 
-void Jacobi(double** _mesh, int _rows, int _cols) {
-
+double exactSolution(double _x, double _y) {
+	return (1 - _x) * _x * sin(Pi * _y);
 }
+
+double** rightPart(double _step, double _rows, double _cols, double _k) {
+	double** result = createMatr(_rows, _cols);
+	for (int i = 1; i < _rows - 1; ++i) {
+		for (int j = 1; j < _cols - 1; ++j) {
+			result[i][j] = f(i * _step, j * _step, _k);
+		}
+	}
+	double boundaryValue = 0.0;
+	/*leftBoundary(result, _rows, _cols, boundaryValue);
+	rightBoundary(result, _rows, _cols, boundaryValue);
+	topBoundary(result, _rows, _cols, boundaryValue);
+	bottomBoundary(result, _rows, _cols, boundaryValue);*/
+	return result;
+}
+
+void Jacobi(double** _mesh, int _rows, int _cols, double _k, double _step) {
+	double** rPart = rightPart(_step, _rows, _cols, _k);
+	double c = 1 / (4 + _k * _k *_step*_step);
+	//double** previousLayer = nullptr;
+	for (int s = 0; s < 10000; ++s) {
+		//cout << "_mesh[1][1] = " << _mesh[1][1] << endl;
+		//cout << "_mesh[2][1] = " << _mesh[2][1] << endl;
+		double** previousLayer = copyMesh(_mesh, _rows, _cols);
+		//cout << "previousLayer = " << previousLayer[1][1] << endl;
+		for (int i = 1; i < _rows - 1; ++i) {
+			for (int j = 1; j < _cols - 1; ++j) {
+				_mesh[i][j] = c * (previousLayer[i - 1][j] + previousLayer[i + 1][j] + previousLayer[i][j - 1] + \
+					previousLayer[i][j + 1] + _step * _step*rPart[i][j]);
+			}
+		}
+		deleteMatr(previousLayer, _rows);
+	}
+	if (checkResult(_mesh, _rows, _cols, _step)) {
+		cout << "Answer is correct" << endl;
+	}
+	else {
+		cout << "Answer is INcorrect" << endl;
+	}
+	deleteMatr(rPart, _rows);
+	//deleteMatr(previousLayer, _rows);
+}
+
+//void Jacobi(double** _mesh, int _rows, int _cols, double _k, double _step) {
+//	double** rPart = rightPart(_step, _rows, _cols, _k);
+//	double c = 1 / (4 + _k * _k *_step*_step);
+//	double temp = 0;
+//	//double** previousLayer = nullptr;
+//	for (int s = 0; s < 100000; ++s) {
+//		//cout << "_mesh[1][1] = " << _mesh[1][1] << endl;
+//		//cout << "_mesh[99][99] = " << _mesh[99][99] << endl;
+//		double** previousLayer = copyMesh(_mesh, _rows, _cols);
+//		//cout << "previousLayer = " << previousLayer[1][1] << endl;
+//		for (int i = 1; i < _rows / 2; ++i) {
+//			for (int j = 1; j < _cols - 1; ++j) {
+//				temp = c * (previousLayer[i - 1][j] + previousLayer[i + 1][j] + previousLayer[i][j - 1] + \
+//					previousLayer[i][j + 1] + _step * _step*rPart[i][j]);
+//				_mesh[i][j] = temp;
+//				_mesh[_rows - 1 - i][_cols - 1 - j] = temp;
+//				//cout << i << " " << _rows - 1 - i << endl;
+//			}
+//			for (int i = _rows / 2; i < _rows / 2 + 1; ++i) {
+//				for (int j = 1; j < _cols - 1; ++j) {
+//					_mesh[i][j] = c * (previousLayer[i - 1][j] + previousLayer[i + 1][j] + previousLayer[i][j - 1] + \
+//						previousLayer[i][j + 1] + _step * _step*rPart[i][j]);
+//				}
+//			}
+//
+//		}
+//		deleteMatr(previousLayer, _rows);
+//	}
+//	if (checkResult(_mesh, _rows, _cols, _step)) {
+//		cout << "Answer is correct" << endl;
+//	}
+//	else {
+//		cout << "Answer is INcorrect" << endl;
+//	}
+//	deleteMatr(rPart, _rows);
+//	//deleteMatr(previousLayer, _rows);
+//}
+
+
 
 // Additional
 
@@ -37,33 +120,62 @@ void zeroLayer(double** _mesh, int _rows, int _cols) {
 			_mesh[i][j] = zeroValue;
 		}
 	}
-	leftBoundary(_mesh, _rows, _cols);
-	rightBoundary(_mesh, _rows, _cols);
-	topBoundary(_mesh, _rows, _cols);
-	bottomBoundary(_mesh, _rows, _cols);
+	double boundaryValue = 0.0;
+	/*leftBoundary(_mesh, _rows, _cols, boundaryValue);
+	rightBoundary(_mesh, _rows, _cols, boundaryValue);
+	topBoundary(_mesh, _rows, _cols, boundaryValue);
+	bottomBoundary(_mesh, _rows, _cols, boundaryValue);*/
+}
+
+double** copyMesh(double** _mesh, int _rows, int _cols) {
+	double** result = createMatr(_rows, _cols);
+	for (int i = 0; i < _rows; ++i) {
+		for (int j = 0; j < _cols; ++j) {
+			result[i][j] = _mesh[i][j];
+		}
+	}
+	return result;
+}
+
+bool checkResult(double** _result, int _rows, int _cols, double _step) {
+	double epsNull = 1e-2;
+	for (int i = 0; i < _rows; ++i) {
+		for (int j = 0; j < _cols; ++j) {
+			if (fabs(_result[i][j] - exactSolution(i * _step, j * _step)) > epsNull) {
+				cout << endl;
+				cout << "_result[i][j] = " << _result[i][j] << endl;
+				cout << "Exact = " << exactSolution(i * _step, j * _step) << endl;
+				cout << "i = " << i << endl;
+				cout << "j = " << j << endl;
+				cout << endl;
+				return false;
+			}
+		}
+	}
+	return true;
 }
 
 // Boundaries
 
-void leftBoundary(double** _mesh, int _rows, int _cols, double _boundValue = 0.0) {
+void leftBoundary(double** _mesh, int _rows, int _cols, double _boundValue) {
 	for (int i = 0; i < _rows; ++i) {
 		_mesh[i][0] = _boundValue;
 	}
 }
 
-void rightBoundary(double** _mesh, int _rows, int _cols, double _boundValue = 0.0) {
+void rightBoundary(double** _mesh, int _rows, int _cols, double _boundValue) {
 	for (int i = 0; i < _rows; ++i) {
 		_mesh[i][_cols - 1] = _boundValue;
 	}
 }
 
-void topBoundary(double** _mesh, int _rows, int _cols, double _boundValue = 0.0) {
+void topBoundary(double** _mesh, int _rows, int _cols, double _boundValue) {
 	for (int i = 0; i < _cols; ++i) {
 		_mesh[_rows - 1][i] = _boundValue;
 	}
 }
 
-void bottomBoundary(double** _mesh, int _rows, double _cols, double _boundValue = 0.0) {
+void bottomBoundary(double** _mesh, int _rows, double _cols, double _boundValue) {
 	for (int i = 0; i < _cols; ++i) {
 		_mesh[0][i] = _boundValue;
 	}
--- project/src/main.cpp
+++ project/src/main.cpp
@@ -6,8 +6,10 @@ using namespace std;
 int main() {
 	double rows = 0.0;
 	double cols = 0.0;
-	double** testMesh = createMesh(1, 1, 0.5, &rows, &cols);
-	printMatr(testMesh, rows, cols);
-	deleteMatr(testMesh, rows);
+	double step = 0.01;
+	double** mesh = createMesh(1, 1, step, &rows, &cols);
+	Jacobi(mesh, rows, cols, 1, step);
+	//printMatr(mesh, rows, cols);
+	deleteMatr(mesh, rows);
 	system("pause");
 }
